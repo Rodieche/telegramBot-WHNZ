dee2d98b44e23964f0cd0d3022b63062b7de0718
Rodolfo Echenique <rechenique@MacBook-Pro-de-Rodolfo.local>
2023-12-31 09:07:37 +1300

    Feat: Se mejora el saludo de bienvenida al bot y se añade el comando help

--- main.js
+++ main.js
@@ -8,10 +8,27 @@ const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 const MAX_MESSAGE_LENGTH = 4000;
 
+const commandList = `/recruitments - Lista de reclutadoras
+    
+/exchange - Valor del cambio del dolar Estadounidense
+
+/firststeps - Primeros pasos al llegar como WH
+
+/faq - Preguntas frecuentes
+
+/about - Aceca del bot WHNZ
+
+/help - Lista de comandos`
+
 // Manejar comandos
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    const greetMessage = "¡Kia ora! Bienvenido a la aplicación de Nueva Zelanda diseñada para ayudar a los entusiastas de las working holidays a obtener información inicial crucial sobre Nueva Zelanda. Estamos aquí para proporcionarte conocimientos valiosos y recursos que mejorarán tu experiencia durante tu working holiday. Siéntete libre de explorar y descubrir todo lo que necesitas saber sobre vivir, trabajar y disfrutar tu tiempo en Nueva Zelanda. Si tienes alguna pregunta, no dudes en preguntar. ¡Feliz exploración!"
+    const greetMessage = `¡Kia ora! Bienvenido a la aplicación de Nueva Zelanda diseñada para ayudar a los entusiastas de las working holidays a obtener información inicial crucial sobre Nueva Zelanda. Estamos aquí para proporcionarte conocimientos valiosos y recursos que mejorarán tu experiencia durante tu working holiday. Siéntete libre de explorar y descubrir todo lo que necesitas saber sobre vivir, trabajar y disfrutar tu tiempo en Nueva Zelanda. Si tienes alguna pregunta, no dudes en preguntar. ¡Feliz exploración!
+    
+    Para comenzar podes explorar nuestras lista de comandos:
+
+    ${commandList}
+    `
     bot.sendMessage(chatId, greetMessage);
 });
 
@@ -213,4 +230,25 @@ bot.onText(/\/about/, async (msg) => {
     }
 
     bot.sendMessage(chatId, text, options);
+});
+
+bot.onText(/\/help/, async (msg) => {
+    const chatId = msg.chat.id;
+    const chatType = msg.chat.type;
+
+    const options = {
+        disable_web_page_preview: true,
+        parse_mode: 'HTML'
+    };
+
+    if (chatType === 'group' || chatType === 'supergroup') {
+        // Si es un grupo, intentamos obtener el ID del mensaje original
+        const replyToMessageId = msg.reply_to_message ? msg.reply_to_message.message_id : null;
+
+        if (replyToMessageId) {
+            options.reply_to_message_id = replyToMessageId;
+        }
+    }
+
+    bot.sendMessage(chatId, commandList, options);
 });
